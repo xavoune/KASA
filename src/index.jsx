7b27bdc210ad291c2b2e948416f224d7ffef328e
Xavoune <lespade.xavier@gmail.com>
2024-02-06 18:43:17 +0100

    import react-rooter + création routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Accomodation from './pages/Acomodations'
+import About from './pages/About'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Error from './components/Error'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/accomodations" element={<Accomodation />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
