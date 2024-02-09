15899cdd258b63d1b2936a343fda69e80c3a3256
Xavoune <lespade.xavier@gmail.com>
2024-02-09 12:09:29 +0100

    passage react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client' // Importe createRoot
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import '../src/styles/main.scss'
 import Home from './pages/Home'
@@ -9,22 +9,26 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Error from './components/Error'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/accomodations"
-          /* ajouter l'id du logement pour rediriger vers le bon logement */
-          element={<Accomodation />}
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+// Utilise createRoot pour le rendu
+const container = document.getElementById('root')
+if (container !== null) {
+  const root = createRoot(container) // Crée une racine pour l'application
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/accomodations"
+            /* ajouter l'id du logement pour rediriger vers le bon logement */
+            element={<Accomodation />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </React.StrictMode>,
+  )
+}
