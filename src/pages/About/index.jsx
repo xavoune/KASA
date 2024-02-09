69b22a47e4933c9a308604c887260cef6d5a2d02
Xavoune <lespade.xavier@gmail.com>
2024-02-09 18:14:19 +0100

    banner modif

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,15 @@
+import Banner from '../../components/Banner'
+import imgBannerAbout from '../../assets/Kasa_About.png'
+
 function About() {
-  return <div>A Propos 🏡</div>
+  return (
+    <div className="bodyMain">
+      <Banner>
+        <img src={imgBannerAbout} alt="bannière" className="kasa-banner__img" />
+        <div className="kasa-banner__overlay"></div>
+      </Banner>
+    </div>
+  )
 }
 
 export default About
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,17 @@
+import Banner from '../../components/Banner'
 import '../../styles/pages/Home.scss'
+import imgBannerHome from '../../assets/Kasa_Home.png'
 
 function Home() {
-  return <div>Page d'accueil 🏡</div>
+  return (
+    <div className="bodyMain">
+      <Banner>
+        <img src={imgBannerHome} alt="bannière" className="kasa-banner__img" />
+        <div className="kasa-banner__overlay"></div>
+        <h1 className="kasa-banner__title">Chez vous, partout et ailleurs</h1>
+      </Banner>
+    </div>
+  )
 }
 
 export default Home
