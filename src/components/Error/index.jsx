e5b829de2868174379722f1b1f71304693e0330b
Xavoune <lespade.xavier@gmail.com>
2024-02-20 14:59:44 +0100

    finalisation page error

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
+import '../../styles/components/error.scss'
+import errorImg from '../../assets/Kasa_404.png'
 function Error() {
-  return <div>La page est introuvable</div>
+  return (
+    <div className="error__container">
+      <div className="error__body">
+        <div className="error__message">
+          <img
+            src={errorImg}
+            alt="erreur 404"
+            className="error__message__img"
+          />
+          <h1 className="error__message__title">
+            Oups! La page que vous demandez n'existe pas.
+          </h1>
+        </div>
+        <Link to="/">Retourner a la page d'accueil</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Error
