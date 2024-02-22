dda6fcbd3a99e6637a6fbfe20efea5c32add4b3a
Xavoune <lespade.xavier@gmail.com>
2024-02-22 14:42:55 +0100

    création composant collapse

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,7 @@
 import Banner from '../../components/Banner'
 import imgBannerAbout from '../../assets/Kasa_About.png'
+import Collapse from '../../components/Collapse'
+import '../../styles/pages/About.scss'
 
 function About() {
   return (
@@ -8,6 +10,39 @@ function About() {
         <img src={imgBannerAbout} alt="bannière" className="kasa-banner__img" />
         <div className="kasa-banner__overlay"></div>
       </Banner>
+      <div className="aboutWrapper">
+        <Collapse title="Fiabilité">
+          <p>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </p>
+        </Collapse>
+        <Collapse title="Respect">
+          <p>
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </p>
+        </Collapse>
+        <Collapse title="Service">
+          <p>
+            Nos équipes se tiennent à votre disposition pour vous fournir une
+            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+            moindre question.
+          </p>
+        </Collapse>
+        <Collapse title="Sécurité">
+          <p>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+            les standards sont bien respectés. Nous organisons également des
+            ateliers sur la sécurité domestique pour nos hôtes.
+          </p>
+        </Collapse>
+      </div>
     </div>
   )
 }
