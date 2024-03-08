49774f68b81b415c41015e204eda9afbdbeb66e3
Xavoune <lespade.xavier@gmail.com>
2024-03-08 10:21:55 +0100

    import modif accommodation

--- src/pages/Accommodations/index.jsx
+++ src/pages/Accommodations/index.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import accommodationsData from '../../datas/logements.json' // Assurez-vous d'ajuster le chemin selon votre structure de projet
 import Slideshow from '../../components/Carousel'
+import StarRating from '../../components/starRating'
+import Tags from '../../components/Tags'
 import '../../styles/pages/Accommodations.scss'
 
 function Accommodation() {
@@ -25,8 +27,19 @@ function Accommodation() {
       <Slideshow images={accommodation.pictures} />
 
       {/* Maintenant, cette ligne ne causera pas d'erreur car elle est exécutée seulement si accommodation n'est pas null */}
-      <h1>{accommodation.title}</h1>
-      {/* Et ainsi de suite pour les autres propriétés de l'accommodation */}
+      <div className="accommodationBody__title">
+        <div className="accommodationBody__title--location">
+          <h1>{accommodation.title}</h1>
+          <p>{accommodation.location}</p>
+          <Tags tags={accommodation.tags} />
+        </div>
+        <div className="accommodationBody__title--host">
+          <p>{accommodation.host.name}</p>
+          <img src={accommodation.host.picture} alt={accommodation.host.name} />
+          <StarRating rating={accommodation.rating} />
+        </div>
+        {/* Et ainsi de suite pour les autres propriétés de l'accommodation */}
+      </div>
     </div>
   )
 }
